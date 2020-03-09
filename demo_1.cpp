7046847a5ec233bf910ab59fa66807ecaee08b60
wangzhumo <wangzhumoo@gmail.com>
2020-03-09 21:06:36 +0800

    [feature]init project

---
+++ demo_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[]){
+
+    cout << "Hello World." << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    
+
+    return 0;
+}
