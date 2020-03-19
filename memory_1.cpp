84083009a4107b4b3b3126b3da427bb03cd879ed
wangzhumo <wangzhumoo@gmail.com>
2020-03-19 11:54:11 +0800

    [feature]edit memory_1

--- memory_1.cpp
+++ memory_1.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 
 int a = 0;      //(GVAR) 全局初始化区
 int* p1;        //(bss)全局未初始化区
@@ -13,8 +18,15 @@ int main(){        //(text)代码区
     
     char const *p3 = "123456";    //123456 常量区
                                   //p3 (stack)栈区变量 
-    
+
+    static int c = 0 ;            //(GVAR)全局静态初始化区
+    p1 = new int(10);             //heap堆区
+    p2 = new int(20);             //heap堆区
+    char* p4 = new char[7];       //heap堆区
+
+    //strcpy_s(p4,7,"123456");          //(text)代码区
+
 
     
-    return 0;
+    return 0;     //(text)代码区
 }
