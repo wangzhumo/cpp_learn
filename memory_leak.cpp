b9fb314eaf69480714378a59db4072837822ebf5
wangzhumo <wangzhumoo@gmail.com>
2020-04-14 21:42:43 +0800

    [feature]memory_leak

---
+++ memory_leak.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//内存泄露
+//  程序中已经动态分配的堆内存由于一些原因没有释放,或者无法释放
+//  造成系统的内存浪费,导致程序运行速度减慢,甚至程序异常崩溃等后果.
+//
+//智能指针
+//	unique_ptr
+//	shared_ptr
+//	weak_ptr
+//	(deprecated) auto_ptr   C++17
+int main(){
+    
+
+    return 0;
+}
