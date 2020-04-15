3c9196be937a7fa4c5d86a3fc0698e36d68cfd16
wangzhumo <wangzhumoo@gmail.com>
2020-04-16 00:14:36 +0800

    [feature]syntax_if.cpp -2

--- syntax_if.cpp
+++ syntax_if.cpp
@@ -4,6 +4,36 @@
 using namespace std;
 
 
+//简单的if
+bool isRunYear(int year){
+    if ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)  )
+    {
+        return true;
+    }
+    return false;
+}
+
+
+//typedef 表示把 _COLOR 命名为color
+typedef enum _COLOR {
+    RED,
+    GRAY,
+    GREEN,
+    UNKOWN
+}color;
+
+
+//多个分支的if
+void printColor(color colors){
+ if(colors == RED){
+     cout << "红色" << endl;
+ }else if(colors == GRAY){
+     cout << "灰色" << endl;
+ }else{
+     cout << "未知" << endl;
+ }
+}
+
 
 //if语句
 // if(条件) {
