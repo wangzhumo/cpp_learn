57ba6ca5f6c6ff599228f5e3121ee01ba8b3b51c
wangzhumo <wangzhumoo@gmail.com>
2020-04-15 15:26:45 +0800

    [feature]unique_prt

--- unique_ptr_1.cpp
+++ unique_ptr_1.cpp
@@ -11,10 +11,25 @@ using namespace std;
 //
 // 如果一定要使用,因为unique_ptr禁止拷贝语义,我们可以使用 std::move()进行控制权转移
 int main(){
+    
+    //定义范围
+    {
+        auto q1 = unique_ptr<int>(new int(12));
+        
+    }
+    //当程序运行到这里,说明 q1 已经出了它的作用范围,此时就已经被释放了.
+
 
     // unique_ptr
-    auto w = std::make_unique<int>(10);
+    auto w = std::unique_ptr<int>(new int(10));
+    cout << *(w.get()) << endl;
 
+    //unique_ptr  只支持通过move()指令来进行赋值,转移控制权
+    auto w2 = std::move(w);
+    //此时w2获得内存的所有权,而之前的w == nullptr
+
+    cout << ((w.get() != nullptr)? *(w.get()) : -1 ) << endl;
+    cout << ((w2.get() != nullptr)? *(w2.get()) : -1 ) << endl;
 
     return 0;
 }
