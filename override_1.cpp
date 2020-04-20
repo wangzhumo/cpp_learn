04ba8f0adeff812498705ea226203dc79d7caa9d
wangzhumo <wangzhumoo@gmail.com>
2020-04-20 23:37:44 +0800

    [feature]override -3

--- override_1.cpp
+++ override_1.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+
+//函数重载 与 C++的 Name Mangling
+int test(int a);
+
+int test(double a);
+
+int test(int a ,int b);
+// ?test@@YAHH@Z
+// ?test@@YAHN@Z
+// ?test@@YAHHN@Z
+
+
+
+
 //override
 int main(){
+
     return 0;
 }
