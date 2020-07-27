756a6408fc8863bcc8189bb822fac64aeb923459
wangzhumo <wangzhumoo@gmail.com>
2020-07-27 23:33:43 +0800

    [feature]迭代器

---
+++ includes/advance/stl_iterator.h
@@ -0,0 +1,16 @@
+//
+// Created by wangzhumo on 2020/7/27.
+//
+
+#ifndef LEARN_CPP_STL_ITERATOR_H
+#define LEARN_CPP_STL_ITERATOR_H
+
+// 正向迭代器   iterator
+// 常量迭代器   const_iterator  也就是说，这个迭代器的元素不可以修改它
+// 反向迭代器   reverse_iterator
+// 反向常量迭代器    const_reverse_iterator
+
+
+
+
+#endif //LEARN_CPP_STL_ITERATOR_H
