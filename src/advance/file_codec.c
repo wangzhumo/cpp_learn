87787786d58747616036fc1ef529c48fb4b7f7e0
wangzhumo <wangzhumoo@gmail.com>
2020-07-31 00:08:23 +0800

    [feature]文件的加密 -3

--- src/advance/file_codec.c
+++ src/advance/file_codec.c
@@ -17,21 +17,26 @@ void encodeFile(const char *fileName,const char  *dstFile){
     // 循环加密
     char item;
     // 判断读出来的元素，不是结尾
-    while ((item = fgetc(inputFile)) != EOF){
-        // 自己做一个加密的操作
-        // 加长这个char,以便于做加密，保存所有数据 ->  转为 short
-        // 35    0000 0000 0010 0011   元数据
-        // << 4  0000 0010 0011 0000   加密数据
-        short temp = (short) item;
-        // 对它操作
-        temp = temp << 4;
-        // << 4   0000 0010 0011 0000   加密数据
-        //|0x8000 1000 0000 0000 0000
-        //        1000 0010 0011 0000
-        temp = temp | 0x8000;
-        fprintf(outputFile,"%hd",temp);
+//    while ((item = fgetc(inputFile)) != EOF){
+//        // 自己做一个加密的操作
+//        // 加长这个char,以便于做加密，保存所有数据 ->  转为 short
+//        // 35    0000 0000 0010 0011   元数据
+//        // << 4  0000 0010 0011 0000   加密数据
+//        short temp = (short) item;
+//        // 对它操作
+//        temp = temp << 4;
+//        // << 4   0000 0010 0011 0000   加密数据
+//        //|0x8000 1000 0000 0000 0000
+//        //        1000 0010 0011 0000
+//        temp = temp | 0x8000;
+//        fprintf(outputFile,"%hd",temp);
+//    }
+    char line[1024];
+    while (!feof(outputFile)){
+        memset(line,0,1024);
+        fgets(line,1000,outputFile);
+        printf("%s",line);
     }
-
 
     fclose(inputFile);
     fclose(outputFile);
