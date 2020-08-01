d2ef42e74e7761bf567e3b8b97a9b0fec0a3ab17
wangzhumo <wangzhumoo@gmail.com>
2020-08-01 22:42:01 +0800

    [feature]shell demo -1

---
+++ shell/shell_demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+#File by wangzhumo
+echo "wangzhumo"
+
+echo "###############"
+echo $0
+echo $1
+echo $2
+
+echo "###############"
+
+echo "this \$? is $?"
+echo "this \*? is $*"
+echo "this \#? is $#"
