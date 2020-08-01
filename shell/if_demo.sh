0521059e17072ce7e8f4ecc8a21eb0cac5d837c4
wangzhumo <wangzhumoo@gmail.com>
2020-08-01 22:44:06 +0800

    [feature]shell function if

---
+++ shell/if_demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NUM1=100
+NUM2=200
+if(($NUM1>$NUM2));then
+	echo "num1 > num2"
+else 
+	echo "num1 < num2"
+fi
+
+
+if [ ! -d /home/wangzhumo/code/shell/empty ]; then
+	mkdir -p /home/wangzhumo/code/shell/empty
+else
+	echo "dir already exist"
+fi
