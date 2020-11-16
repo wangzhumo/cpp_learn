cd3a5107c302f2c6ce90b7ce6181236954565a47
wangzhumo <wangzhumoo@gmail.com>
2020-11-16 15:33:18 +0800

    [feature]排序测试帮助类

--- includes/sorts/sort_test_helper.h
+++ includes/sorts/sort_test_helper.h
@@ -9,10 +9,11 @@
 #include <ctime>
 #include <cassert>
 
-namespace SortTestHelper{
+namespace SortTestHelper {
+
     // 产生随机数
-    int* generateRandomArray(int n,int start,int limit){
-        assert(start<limit);
+    int *generateRandomArray(int n, int start, int limit) {
+        assert(start < limit);
         int *arr = new int[n];
         //set random seed
         srand(time(NULL));
@@ -23,12 +24,35 @@ namespace SortTestHelper{
     }
 
     // 打印
-    template<typename  T>
-    void printArray(T arr[],int n){
+    template<typename T>
+    void printArray(T arr[], int n) {
         for (int i = 0; i < n; i++) {
             std::cout << arr[i] << " ";
         }
         std::cout << std::endl;
+    }
+
+    // 是否大小顺序
+    template<typename T>
+    bool isSorted(T arr[], int n) {
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 测试
+    template<typename T>
+    void testSort(std::string sortName, void(*sort)(T[], int), T arr[], int n) {
+        std::clock_t startTime = clock();
+        sort(arr, n);
+        std::clock_t endTime = clock();
+
+        // 是否排序成功的？
+        assert(isSorted(arr,n));
+        std::cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << std::endl;
     }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include "../src/algorithm/sorts/select_sort.cpp"
 #include "../includes/sorts/sort_test_helper.h"
+#include "../includes/sorts/select_sort.h"
+
 #include <iostream>
 
 int main() {
     int n  = 10000;
     int *array = SortTestHelper::generateRandomArray(n,0,n);
-    selectionSort<int>(array, n);
-    SortTestHelper::printArray(array,n);
+    SortTestHelper::testSort("Selection Sort",selectionSort,array,n);
 
     delete [] array;
     return 0;
