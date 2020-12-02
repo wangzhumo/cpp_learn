91ccd8ef34054f04470bb1229ea7b15fce74931c
wangzhumo <wangzhumoo@gmail.com>
2020-12-02 23:30:44 +0800

    [feature]max_heap shiftUp insert

--- includes/heap/max_heap.h
+++ includes/heap/max_heap.h
@@ -5,13 +5,21 @@
 #ifndef LEARN_CPP_MAX_HEAP_H
 #define LEARN_CPP_MAX_HEAP_H
 
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
 template<class Item>
 class MaxHeap {
 private:
     Item *data;  //数据
     int count;   //当前个数
+    int capacity; //容量
 public:
-    MaxHeap(int capacity);
+    explicit MaxHeap(int capacity);
 
     ~MaxHeap();
 
@@ -20,6 +28,115 @@ public:
 
     // 是否为空
     bool isEmpty() { return count == 0; }
+
+    // 添加元素
+    int insert(Item e);
+
+    // 获取一个元素
+    Item pop();
+
+    // 以树状打印整个堆结构
+    void testPrint() {
+        // 我们的testPrint只能打印100个元素以内的堆的树状信息
+        if (size() >= 100) {
+            std::cout << "This print function can only work for less than 100 int";
+            return;
+        }
+
+        // 我们的testPrint只能处理整数信息
+        if (typeid(Item) != typeid(int)) {
+            std::cout << "This print function can only work for int item";
+            return;
+        }
+
+        std::cout << "The max heap size is: " << size() << std::endl;
+        std::cout << "Data in the max heap: ";
+        for (int i = 1; i <= size(); i++) {
+            // 我们的testPrint要求堆中的所有整数在[0, 100)的范围内
+            assert(data[i] >= 0 && data[i] < 100);
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+        int n = size();
+        int max_level = 0;
+        int number_per_level = 1;
+        while (n > 0) {
+            max_level += 1;
+            n -= number_per_level;
+            number_per_level *= 2;
+        }
+
+        int max_level_number = int(pow(2, max_level - 1));
+        int cur_tree_max_level_number = max_level_number;
+        int index = 1;
+        for (int level = 0; level < max_level; level++) {
+            std::string line1 = std::string(max_level_number * 3 - 1, ' ');
+
+            int cur_level_number = std::min(count - int(pow(2, level)) + 1, int(pow(2, level)));
+            bool isLeft = true;
+            for (int index_cur_level = 0; index_cur_level < cur_level_number; index++, index_cur_level++) {
+                putNumberInLine(data[index], line1, index_cur_level, cur_tree_max_level_number * 3 - 1, isLeft);
+                isLeft = !isLeft;
+            }
+            std::cout << line1 << std::endl;
+
+            if (level == max_level - 1)
+                break;
+
+            std::string line2 = std::string(max_level_number * 3 - 1, ' ');
+            for (int index_cur_level = 0; index_cur_level < cur_level_number; index_cur_level++)
+                putBranchInLine(line2, index_cur_level, cur_tree_max_level_number * 3 - 1);
+            std::cout << line2 << std::endl;
+
+            cur_tree_max_level_number /= 2;
+        }
+    }
+
+
+
+private:
+    /**
+     *  排序新元素，使得这个新添加的元素满足完全堆的定义
+     */
+    void shiftUp(int index);
+
+    /**
+     *  移除一个元素
+     */
+    void shiftDown(int index);
+
+
+
+    void putNumberInLine(int num, std::string &line, int index_cur_level, int cur_tree_width, bool isLeft) {
+
+        int sub_tree_width = (cur_tree_width - 0) / 2;
+        int offset = index_cur_level * (cur_tree_width + 0) + sub_tree_width;
+        assert(offset + 0 < line.size());
+        if (num >= 9) {
+            line[offset + -1] = '0' + num / 10;
+            line[offset + 0] = '0' + num % 10;
+        } else {
+            if (isLeft)
+                line[offset + -1] = '0' + num;
+            else
+                line[offset + 0] = '0' + num;
+        }
+    }
+
+    void putBranchInLine(std::string &line, int index_cur_level, int cur_tree_width) {
+
+        int sub_tree_width = (cur_tree_width - 0) / 2;
+        int sub_sub_tree_width = (sub_tree_width - 0) / 2;
+        int offset_left = index_cur_level * (cur_tree_width + 0) + sub_sub_tree_width;
+        assert(offset_left + 0 < line.size());
+        int offset_right = index_cur_level * (cur_tree_width + 0) + sub_tree_width + 1 + sub_sub_tree_width;
+        assert(offset_right < line.size());
+
+        line[offset_left + 0] = '/';
+        line[offset_right + -1] = '\\';
+    }
 };
 
 #endif  // LEARN_CPP_MAX_HEAP_H
--- src/algorithm/heap/max_heap.cpp
+++ src/algorithm/heap/max_heap.cpp
@@ -4,11 +4,13 @@
 
 #include "../../../includes/heap/max_heap.h"
 
+
 template<class Item>
 MaxHeap<Item>::MaxHeap(int capacity) {
     // 初始化开辟的元素个数
     data = new Item[capacity + 1];
     count = 0;
+    this->capacity = capacity;
 }
 
 template<class Item>
@@ -16,3 +18,54 @@ MaxHeap<Item>::~MaxHeap() {
     // 删除数据
     delete[] data;
 }
+
+template<class Item>
+int MaxHeap<Item>::insert(Item e) {
+    assert(capacity > (count+1));
+    data[count+1] = e;
+    count++;
+    shiftUp(count);
+    return count;
+}
+
+// 一个新元素,自下而上的比较
+template<class Item>
+void MaxHeap<Item>::shiftUp(int index) {
+    // 添加新的元素
+    // 一直比较，直到自己小于父元素
+    while (index > 1 && data[index / 2] < data[index]) {
+        //如果自己大于了父元素，那么交换位置
+        std::swap(data[index], data[index / 2]);
+        // 修改当前位置
+        index /= 2;
+    }
+}
+
+// 已经移除的顶点，此时顶点的元素不满足最大堆
+// 需要自上而下的移动
+template<class Item>
+void MaxHeap<Item>::shiftDown(int index) {
+
+}
+
+// 取出最大值
+template<class Item>
+Item MaxHeap<Item>::pop() {
+    assert(count > 0);
+    Item item = data[1]; // 因为添加的时候，是从1开始添加的
+    // 把最后一个元素移动到第一个
+    std::swap(data[1],data[count]);
+    // count--
+    count--;
+    // 重新排序
+    shiftDown(1);
+    return item;
+}
+
+
+// Explicit template instantiation
+template
+class MaxHeap<int>;
+
+template
+class MaxHeap<float>;
