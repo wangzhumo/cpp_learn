da686d22e1a586db4ff0727571a5f64f45ebee42
wangzhumo <wangzhumoo@gmail.com>
2020-12-02 21:27:24 +0800

    [feature]Cmake + max_heap

--- includes/heap/max_heap.h
+++ includes/heap/max_heap.h
@@ -5,10 +5,21 @@
 #ifndef LEARN_CPP_MAX_HEAP_H
 #define LEARN_CPP_MAX_HEAP_H
 
+template<class Item>
+class MaxHeap {
+private:
+    Item *data;  //数据
+    int count;   //当前个数
+public:
+    MaxHeap(int capacity);
 
-class max_heap {
+    ~MaxHeap();
 
+    // 当前容量
+    int size() { return count; }
+
+    // 是否为空
+    bool isEmpty() { return count == 0; }
 };
 
-
-#endif //LEARN_CPP_MAX_HEAP_H
+#endif  // LEARN_CPP_MAX_HEAP_H
--- src/algorithm/heap/max_heap.cpp
+++ src/algorithm/heap/max_heap.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "../../../includes/heap/max_heap.h"
+
+template<class Item>
+MaxHeap<Item>::MaxHeap(int capacity) {
+    // 初始化开辟的元素个数
+    data = new Item[capacity + 1];
+    count = 0;
+}
+
+template<class Item>
+MaxHeap<Item>::~MaxHeap() {
+    // 删除数据
+    delete[] data;
+}
