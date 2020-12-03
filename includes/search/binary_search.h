0857dab2d6a5b7e19d9dd01e4cc29bc2c5159c47
wangzhumo <wangzhumoo@gmail.com>
2020-12-03 14:59:16 +0800

    [feature]binary_search

---
+++ includes/search/binary_search.h
@@ -0,0 +1,20 @@
+//
+// Created by 王诛魔 on 12/3/20.
+//
+
+#ifndef LEARN_CPP_BINARY_SEARCH_H
+#define LEARN_CPP_BINARY_SEARCH_H
+
+template <class T>
+class BinarySearch {
+
+public:
+    BinarySearch() = default;
+
+    ~BinarySearch() = default;
+
+    int binarySearch(T aar[],int n,T target)
+};
+
+
+#endif //LEARN_CPP_BINARY_SEARCH_H
---
+++ src/algorithm/search/binary_search.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 王诛魔 on 12/3/20.
+//
+
+#include "../../../includes/search/binary_search.h"
+
+
+template<class T>
+int BinarySearch<T>::binarySearch(T *aar, int n, T target) {
+    // 首先定义查找的区间 [l,r] 前闭后闭
+    int l = 0;
+    int r = n - 1;
+    // 循环并查找,如果相遇还没有退出，则不存在
+    while (l <= r) {
+        int mid = (r - l) / 2 + l;
+        // 开始判断
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            // 如果中间值比自己要小，则应该移动mid的位置
+            l = mid + 1;
+        } else {
+            // 此处的-1，是因为前面已经确定mid != target
+            r = mid - 1;
+        }
+    }
+    return -1;
+}
+
+// Explicit template instantiation
+template
+class BinarySearch<int>;
