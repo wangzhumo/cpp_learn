a4265564eadf1993abfd07aee9f0aae982ec496c
wangzhumo <wangzhumoo@gmail.com>
2020-12-06 23:19:18 +0800

    [feature]leetcode moveZeros

---
+++ includes/leetcode/move_zeroes.h
@@ -0,0 +1,14 @@
+//
+// Created by wangzhumo on 2020/12/6.
+//
+
+#ifndef LEARN_CPP_MOVE_ZEROES_H
+#define LEARN_CPP_MOVE_ZEROES_H
+
+
+class MoveZeroes {
+
+};
+
+
+#endif //LEARN_CPP_MOVE_ZEROES_H
---
+++ src/leetcode/move_zeroes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wangzhumo on 2020/12/6.
+//
+
+#include "../../includes/leetcode/move_zeroes.h"
