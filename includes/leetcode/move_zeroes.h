32952d3e57cfcd11f1e0562f6b22e64b3d345589
wangzhumo <wangzhumoo@gmail.com>
2020-12-07 00:12:05 +0800

    [feature]leetcode moveZeros -1

--- includes/leetcode/move_zeroes.h
+++ includes/leetcode/move_zeroes.h
@@ -11,7 +11,7 @@
 class MoveZeroes {
 
 public:
-    void moveZeroes();
+    void moveZeroes(std::vector<int> &nums);
 };
 
 
--- src/leetcode/move_zeroes.cpp
+++ src/leetcode/move_zeroes.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "../../includes/leetcode/move_zeroes.h"
+
+// 把一段带0的序列中的0全部移动到尾部
+void MoveZeroes::moveZeroes(std::vector<int> &nums) {
+    // 创建一个新的序列
+    std::vector<int> vec;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i]) {
+            vec.push_back(nums[i]);
+        }
+    }
+    // 把序列补全
+    int size = vec.size();
+    for (int j = 0; j < nums.size(); j++) {
+        if (j > size) {
+            nums[j] = 0;
+        } else {
+            nums[j] = vec[j];
+        }
+    }
+}
